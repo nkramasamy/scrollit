1690b9715cc59943b1feae4d1f97389dbd9c4648
Anurag Soni <anuragsoni@microsoft.com>
2021-10-12 21:51:49 +0530

    add logic to get/set ppt scroll

--- Extension/content.js
+++ Extension/content.js
@@ -1,5 +1,6 @@
 $(window).on('load', function() {
-    console.log("Loaded",$("#WACViewPanel"));
+    // Word
+    console.log("Loaded Word ", $("#WACViewPanel"));
 
     if($("#WACViewPanel") == null || $("#WACViewPanel") == undefined)
     {
@@ -21,5 +22,38 @@ $(window).on('load', function() {
         var scrollY = $("#WACViewPanel").scrollTop();
         console.log(scrollY);
         sessionStorage.setItem("scroll", scrollY);
+    });
+
+    // Powerpoint
+    console.log("Loaded Powerpoint ", $("#EditingThumbnailsPanel"));
+
+    if($("#EditingThumbnailsPanel") == null || $("#EditingThumbnailsPanel") == undefined)
+    {
+        return;
+    }
+
+    // select the closest slide
+    var arr = $("#EditingThumbnailsPanel").children().children();
+    var selectedSlideIdx = 0;
+    var currPosPpt = sessionStorage.getItem("scrollPpt");
+    if (arr != null && currPosPpt != null)
+    {
+        console.log("currPosPpt: ", currPosPpt);
+        for(selectedSlideIdx = 0; selectedSlideIdx < arr.length; selectedSlideIdx++) {
+            // get first slide whose topOffset is greater then currPosPpt
+            console.log("slide ", selectedSlideIdx, " offset: ", arr[selectedSlideIdx].offsetTop);
+            if (arr[selectedSlideIdx].offsetTop >= currPosPpt)
+            {
+                break;
+            }
+        }
+        console.log("Selected selectedSlideIdx", selectedSlideIdx+1);
+        if (arr[selectedSlideIdx]) arr[selectedSlideIdx].click();
+    }
+
+    $("#EditingThumbnailsPanel").scroll(function() {
+        console.log("Scroll to: ", $("#EditingThumbnailsPanel").scrollTop());
+        sessionStorage.setItem("scrollPpt",
+            $("#EditingThumbnailsPanel").scrollTop());
     });
 });
