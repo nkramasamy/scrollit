303066ce9b0cc5166b2f60c1bde52a5e10304c1e
Naveen Krishnan Ramasamy <naveenkr@microsoft.com>
2021-10-13 16:03:11 +0530

    Updated to use an inmem cache to store position

--- API/src/index.ts
+++ API/src/index.ts
@@ -1,15 +1,20 @@
 import express from 'express';
 
+const NodeCache = require( "node-cache" );
+const myCache = new NodeCache();
+
 const port=process.env.PORT || 8000
 
 const app = express();
 
+
 app.get('/scrollposition/:url', (req, res) => {
-    res.send(req.params.url);
+    res.send(myCache.get(req.params.url));
 });
 
 app.post('/updatescrollposition/:url/:position', (req, res) => {
-    res.send(req.params);
+    myCache.set( req.params.url, req.params.position, 100000 );
+    res.send(req.params.url);
 });
 
 app.listen(port, () => {
