8fc0c752d9d0bff0c40995b335805e4392670dde
Anurag Soni <anuragsoni@microsoft.com>
2021-10-13 18:29:32 +0530

    pottential fix for CORS error: No 'Access-Control-Allow-Origin' header

--- API/src/index.ts
+++ API/src/index.ts
@@ -4,6 +4,17 @@ const port=process.env.PORT || 8000
 
 const app = express();
 
+app.use((cli, res, next) => {
+    res.setHeader('Allow', "*")
+    res.setHeader('Connection', "keep-alive")
+    res.setHeader("Date", Date())
+    res.setHeader("Content-Type", "application/json; charset=utf-8")
+    res.setHeader('Access-Control-Allow-Origin', "*")
+    res.setHeader('Allow-Control-Allow-Methods', "*")
+    res.setHeader('Access-Control-Allow-Headers', "Origin, X-Requested-With, Content-Type, Accept")
+    next()
+});
+
 app.get('/scrollposition/:url', (req, res) => {
     res.send(req.params.url);
 });
