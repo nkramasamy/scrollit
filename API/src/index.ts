db19be8bc0dfc7705eb1ba4f0f75749fd0e4fda6
Naveen Krishnan Ramasamy <naveenkr@microsoft.com>
2021-10-13 14:57:47 +0530

    Added get and post apis, also added build entry in package

--- API/src/index.ts
+++ API/src/index.ts
@@ -1,11 +1,17 @@
 import express from 'express';
 
+const port=process.env.PORT || 8000
+
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Hello World - here an Express project with TS');
+app.get('/scrollposition/:url', (req, res) => {
+    res.send(req.params.url);
 });
 
-app.listen(8000, () => {
-    console.log('The application is listening on port 3000!');
+app.post('/updatescrollposition/:url/:position', (req, res) => {
+    res.send(req.params);
+});
+
+app.listen(port, () => {
+    console.log('The application is listening on port 8000!');
 });
